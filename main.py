a1c2d51d24175eca38e17e36e135e3958b4608fe
Fittter <mcassiani526@gmail.com>
2023-11-16 19:18:22 -0800

    i did stuff

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 
 def print_sys_props():
+    print(sys.
     print(sys.executable)
     print(sys.path)
 
 def main ():
     print (print_sys_props())
-
+    print ("hello world")
     
 main()
+
 
 
